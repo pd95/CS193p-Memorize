e03a0f4e82fabe600ab915bdffaa2bd261d61ffd
Philipp <pd95@users.noreply.github.com>
2020-05-19 18:34:59 +0200

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Philipp on 19.05.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
