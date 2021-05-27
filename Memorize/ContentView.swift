2126b5ef29ce5c9192ef1aa8f54dadb420e04ff8
Philipp <pd95@users.noreply.github.com>
2021-05-27 16:52:30 +0200

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Philipp on 27.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Philipp on 27.05.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
